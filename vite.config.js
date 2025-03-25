c2457b385d89ba855ffa2be7d72d46137b366f83
Rohith723 <rohith6723@gmail.com>
2025-03-25 22:34:10 +0530

    Watch-together

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Watch-together/", // Required for GitHub Pages
+  base: "/Watch-together/", // Ensure this matches your GitHub repo name exactly
 });
